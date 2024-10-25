acd9db3fc9c71b9adffbe7f6a79615ded67b4203
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-10-25 16:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-function daysofAYear(year){
+function daysofaYear(year){
 	if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
 		 return 366; // Leap year
 	}else {
     return 365; // Non-leap year
   }
 }
-console.log(daysOfAYear(2024));
+console.log(daysofaYear(2024));
